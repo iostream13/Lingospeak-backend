74771b19db8457ee3787f8a3c31f609cbab2a9ec
Duong Thanh Hien <hienjeony@Duongs-MacBook-Pro.local>
2023-06-15 13:50:02 +0700

    test

--- test1.py
+++ test1.py
@@ -3,17 +3,39 @@ from pydub import AudioSegment
 import speech_recognition as sr
 
 import pyttsx3
+res = ""
+def split_audio_and_transcribe(audio_file_path, chunk_duration_ms):
 
-# Initialize the recognizer
-r = sr.Recognizer()
-def print_file_content(file_path):
-    audio = AudioSegment.from_file(file_path, format=file_path.split(".")[-1])
-    audio.export("temp.wav", format="wav")
-    recognizer = sr.Recognizer()
-    with sr.AudioFile("temp.wav") as source:
-        audio_data = recognizer.record(source)
-        text = recognizer.recognize_google(audio_data)
-        print(text)
-            
-file_path = "ls.mp3"
-print_file_content(file_path)
+    audio = AudioSegment.from_file(audio_file_path)
+    total_duration = len(audio)
+    start_time = 0
+    end_time = chunk_duration_ms
+
+    while end_time <= total_duration:
+        chunk = audio[start_time:end_time]
+        chunk.export("temp.wav", format="wav")
+
+        recognizer = sr.Recognizer()
+        with sr.AudioFile("temp.wav") as source:
+            audio_data = recognizer.record(source)
+            text = recognizer.recognize_google(audio_data)
+            res = res + " " + text
+
+        start_time = end_time
+        end_time += chunk_duration_ms
+
+    # Xử lý phần cuối cùng có thời lượng khác chunk_duration_ms nếu cần thiết
+    if start_time < total_duration:
+        chunk = audio[start_time:]
+        chunk.export("temp.wav", format="wav")
+
+        recognizer = sr.Recognizer()
+        with sr.AudioFile("temp.wav") as source:
+            audio_data = recognizer.record(source)
+            text = recognizer.recognize_google(audio_data)
+            res = res + " " + text
+audio_file_path = "ls.mp3"
+chunk_duration_ms = 10000
+split_audio_and_transcribe(audio_file_path, chunk_duration_ms)
+print(res)
+
